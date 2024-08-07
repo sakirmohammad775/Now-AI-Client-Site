bb159ab4081e69f387bbbbcab8c635e0b56bd966
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-07 22:32:54 +0600

    add to github

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -13,10 +13,10 @@ const Body = () => {
                     <p>How can I help you today?</p>
                 </div>
                 {/* card */}
-                <div className="grid lg:grid-cols-4">
-                    <div className="h-[200px] w-[200px] bg-black">
-                        <p>Suggest beautiful places to see on an upcoming road trip</p>
-                        <FaRegMessage />
+                <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1">
+                    <div className="h-[200px] w-[200px] bg-gray-200 rounded-xl ">
+                        <p className="text-gray-800 text-[20px] p-4"> Suggest beautiful places to see on an upcoming road trip</p>
+                        <FaRegMessage className="mt-10 ml-32 fo" />
                     </div>
                     <div>
                         <FaRegClock />
