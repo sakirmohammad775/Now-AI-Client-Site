35a8eef83ba5738b82a0749a1cb4304302e4f95d
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-07 23:56:42 +0600

    add to github

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,6 +1,9 @@
 import { FaRegClock, FaRegLightbulb, FaRegMessage } from "react-icons/fa6";
 import Navbar from "../Navbar/Navbar";
 import { HiCodeBracket } from "react-icons/hi2";
+import { ImFilePicture } from "react-icons/im";
+import { MdKeyboardVoice } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
 
 
 const Body = () => {
@@ -28,18 +31,36 @@ const Body = () => {
                     </div>
                     <div className="h-[200px] w-[200px] bg-gray-200 rounded-xl hover:bg-slate-200">
                         <p className="text-gray-800 text-[20px] p-4"> Suggest beautiful places to see on an upcoming road trip</p>
-                        <HiCodeBracket  className="mt-10 ml-40 text-2xl" />
+                        <HiCodeBracket className="mt-10 ml-40 text-2xl" />
                     </div>
-                   
-                    
-                    
+
+
+
                 </div>
             </div>
 
             {/* bottom part */}
-            <div className="max-w-screen-lg mx-auto mt-24  text-center  ">
-            <input type="text" placeholder="Type here" className="input input-bordered w-[90%] rounded-[42px] " />
-            <p className="text-gray-600 mt-3">NowAI may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
+            <div className="max-w-screen-lg mx-auto mt-24 text-center relative">
+                <div className="relative w-[90%] mx-auto">
+                    <input
+                        type="text"
+                        placeholder="Enter a Prompt Here"
+                        className="input input-bordered w-full rounded-[42px] pl-12 pr-12"
+                    />
+                    <div className="absolute top-1/2 right-20 transform -translate-y-1/2 text-2xl text-gray-500">
+                        <ImFilePicture />
+                    </div>
+                    <div className="absolute top-1/2 right-12 transform -translate-y-1/2 text-2xl text-gray-500">
+                        <MdKeyboardVoice />
+                    </div>
+                    <div className="absolute top-1/2 right-3 transform -translate-y-1/2 text-2xl text-gray-500">
+                    <IoMdSend />
+                    </div>
+                </div>
+            </div>
+            <div className="max-w-screen-lg mx-auto text-center ">
+
+                <p className="text-gray-600 mt-3">NowAI may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
             </div>
         </div>
         </>
