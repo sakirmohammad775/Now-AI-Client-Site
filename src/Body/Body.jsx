aa18bad9c59a72d36eb6d25662e34300b50e6697
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-07 22:16:35 +0600

    add to github

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -26,7 +26,10 @@ const Body = () => {
                         <FaRegLightbulb />
                         <p>Suggest beautiful places to see on an upcoming road trip</p>
                     </div>
-                    
+                    <div>
+                        <HiCodeBracket />
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    </div>
                 </div>
             </div>
         </div>
