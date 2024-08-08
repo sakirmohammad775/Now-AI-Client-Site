a9b2704dbe6b61836910dc6e7ee1ea395623cb6f
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-09 00:22:43 +0600

    add to functionality

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -5,24 +5,43 @@ export const Context = createContext()
 
 const ContextProvider = (props) => {
 
-    const [input,setInput]=useState("")
-    const [recentPrompt,setRecentPrompt]=useState("")
-    const [prevPrompts,setPrevPrompts]=useState([])
-    const [showResult,setShowResult]=useState(false)
-    const [loading,setLoading]=useState(false)
-    const [resultData,setResultData]=useState("")
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevPrompts, setPrevPrompts] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState("")
+
+    const dailyPara = (index, nextWord) => {
+
+    }
 
     const onSent = async (prompt) => {
         setResultData('')
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input)
-        const response=await run(input)
+        const response = await run(input)
+
+        let responseArray = response.split("*");
+        let newResponse = "";
+
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i % 2 === 0) { // If index is even
+                newResponse += responseArray[i];
+            } else { // If index is odd
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+
+        let newResponse2 = newResponse.split("*").join("<br/>");
+        setResultData(newResponse2);
+
         setResultData(response)
         setLoading(false)
         setInput("")
     }
-    
+
     const contextValue = {
         prevPrompts,
         setPrevPrompts,
@@ -34,7 +53,7 @@ const ContextProvider = (props) => {
         resultData,
         input,
         setInput,
-        
+
     }
     return (
         <Context.Provider value={contextValue}>
