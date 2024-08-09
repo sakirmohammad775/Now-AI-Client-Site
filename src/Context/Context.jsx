f3d280fb7007227e086bcba8ce7163685d66ecf9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-09 23:02:37 +0600

    add to functionality

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -12,8 +12,10 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
 
-    const dailyPara = (index, nextWord) => {
-
+    const delayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord)
+        }, 75 * index)
     }
 
     const onSent = async (prompt) => {
@@ -21,21 +23,30 @@ const ContextProvider = (props) => {
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input)
+        setPrevPrompts(prev=>[...prev,input])
         const response = await run(input)
 
         let responseArray = response.split("**");
         let newResponse;
 
         for (let i = 0; i < responseArray.length; i++) {
-            if (i === 0 || i % 2 !== 1) { // If index is even
+
+            if (i == 0 || i % 2 == 1) {
                 newResponse += responseArray[i];
-            } else { 
+            } else {
                 newResponse += "<b>" + responseArray[i] + "</b>";
             }
         }
 
-        let newResponse2 = newResponse.split("*").join("<br/>");
-        setResultData(newResponse2);
+        let newResponse2 = newResponse.split("*").join("</br>");
+        let newResponseArray = newResponse2.split(" ");
+
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ");
+        }
+
+
         setLoading(false)
         setInput("")
     }
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
-
 import { CiCirclePlus, CiMenuBurger } from 'react-icons/ci';
 import './Sidebar.css'
 import { FaClockRotateLeft, FaRegCircleQuestion, FaRegMessage } from 'react-icons/fa6';
 import { IoSettings } from 'react-icons/io5';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { Context } from '../Context/Context';
+
 const Sidebar = () => {
     const [extended, setExtended] = useState(false)
+    const [onSent,prevPrompts,setRecentPrompt]=useContext(Context)
     return (
         <>
             <div className='sidebar min-h-[770px] min-w-fit inline-flex flex-col justify-between bg-slate-100 p-5 pt-5'>
@@ -18,13 +20,20 @@ const Sidebar = () => {
                             <CiCirclePlus className="text-xl" />
                             {extended ? <p>New Chat...</p> : null}
                         </div>
-                        {extended ?
+                        {extended 
+                        ?
                             <div className='mt-5'>
                                 <p className='font-semibold transform'>Recent </p>
-                                <div className=' inline-flex gap-2 mt-5 hover:bg-gray-300 py-2 px-2 rounded-[50px]'>
-                                    <FaRegMessage className="text-2xl" />
-                                    <p>What is React ...</p>
-                                </div>
+                                {
+                                    prevPrompts.map((item,index)=>{
+                                        <div className=' inline-flex gap-2 mt-5 hover:bg-gray-300 py-2 px-2 rounded-[50px]'>
+                                        <FaRegMessage className="text-2xl" />
+                                        <p>What is React ...</p>
+                                    </div>
+                                    })
+                                }
+
+                               
                             </div>
                             : null
                         }
