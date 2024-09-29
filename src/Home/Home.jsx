411e69ec4ed5fe69c5a5ffa60373771a0d0ec6f0
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-29 08:49:33 +0600

    delete to the code list

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
         <>
             <div className="max-w-screen-2xl mx-auto ">
                 <div className="w-full flex min-h-screen  duration-[1.5s]">
-                    <Sidebar></Sidebar>
+                    <Sidebar></Sidebar> 
                     <Body></Body>
-                    <h3>NowAI add to home page</h3>
+                    
                 </div>
             </div>
         </>
