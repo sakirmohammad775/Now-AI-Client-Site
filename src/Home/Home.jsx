0a972fd2af4615452744d4ae35a81430ea60a405
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-29 08:53:17 +0600

    add the function

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,7 +9,11 @@ const Home = () => {
                 <div className="w-full flex min-h-screen  duration-[1.5s]">
                     <Sidebar></Sidebar> 
                     <Body></Body>
-                    
+                    <div>
+                        <h3>
+                            NOWAI
+                        </h3>
+                    </div>
                 </div>
             </div>
         </>
